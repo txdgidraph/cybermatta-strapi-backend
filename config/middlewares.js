c6d490ba06c18cc8eef49b8122fa4847e0f9ab28
phinix <tonny@ux-builder.co.uk>
2022-08-28 19:35:44 +0300

    fix media file upload to bucket asset

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,43 @@
+// module.exports = [
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::logger',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            '*.digitaloceanspaces.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            '*.digitaloceanspaces.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
--- config/plugin.js
+++ config/plugin.js
@@ -2,15 +2,20 @@ module.exports = ({env}) => ({
     // ...
     upload: {
       config: {
-        provider: "strapi-provider-upload-do", 
+        provider: 'aws-s3',
         providerOptions: {
-          key: env('DO_SPACE_ACCESS_KEY'),
-          secret: env('DO_SPACE_SECRET_KEY'),
+          accessKeyId: env('DO_SPACE_ACCESS_KEY'),
+          secretAccessKey: env('DO_SPACE_SECRET_KEY'),
           endpoint: env('DO_SPACE_ENDPOINT'),
-          space: env('DO_SPACE_BUCKET'),
-        //   directory: env('DO_SPACE_DIRECTORY'),
-        //   cdn: env('DO_SPACE_CDN'),
-        }
+          params: {
+            Bucket: env('DO_SPACE_BUCKET'),
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
       },
-    }, 
+    },
   })
