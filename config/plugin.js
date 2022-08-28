24876aff65db6d944a0758f6023cfaa168d5bd8e
phinix <tonny@ux-builder.co.uk>
2022-08-28 23:07:05 +0300

    fix media upload issue

--- config/plugin.js
+++ config/plugin.js
@@ -1,21 +1,16 @@
-module.exports = ({env}) => ({
-    // ...
-    upload: {
-      config: {
-        provider: 'aws-s3',
-        providerOptions: {
-          accessKeyId: env('DO_SPACE_ACCESS_KEY'),
-          secretAccessKey: env('DO_SPACE_SECRET_KEY'),
-          endpoint: env('DO_SPACE_ENDPOINT'),
-          params: {
-            Bucket: env('DO_SPACE_BUCKET'),
-          },
-        },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
-        },
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-do",
+      providerOptions: {
+        key: process.env.DO_SPACE_ACCESS_KEY,
+        secret: process.env.DO_SPACE_SECRET_KEY,
+        endpoint: process.env.DO_SPACE_ENDPOINT,
+        space: process.env.DO_SPACE_BUCKET,
+        //directory: "media", // optional
       },
     },
-  })
+  },
+  // ...
+});
